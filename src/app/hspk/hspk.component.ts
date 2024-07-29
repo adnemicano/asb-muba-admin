9968344ec4e4929fddf83330c8a5df21d3e7f447
adnemicano <anandaliladmy@gmail.com>
2024-07-29 11:30:01 +0700

    first commit

---
+++ src/app/hspk/hspk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hspk',
+  templateUrl: './hspk.component.html',
+  styleUrls: ['./hspk.component.scss']
+})
+export class HspkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
